73978b983740ff4fd9ae491d1ff17a985be06bb6
CleomarSilva <nexuscleo@gmail.com>
2026-03-01 22:41:04 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,92 @@
+const convertButton = document.querySelector(".convert-button");
+const currencySelect = document.querySelector(".currency-select");
+
+function convertCurrency() {
+    const inputCurrencyValue = document.querySelector(".input-currency").value;
+    const currencyValueToConvert = document.querySelector(".currency-value");
+    const currencyValueConverted = document.querySelector(".currency-value-converted");
+    const currencyNameMoeda = document.querySelector(".currency-name");
+    const currencyLogoMoeda = document.querySelector(".currency-logo-moeda");
+
+    const dollarToday = 5.25;
+    const euroToday = 6.20;
+    const yuanToday = 0.80;
+    const bitcoinToday = 250000.00;
+
+    if (currencySelect.value === "euro") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR"
+        }).format(inputCurrencyValue / euroToday);
+
+        currencyNameMoeda.innerHTML = "Euro";
+        currencyLogoMoeda.src = "./img/euro.png";
+    }
+
+    if (currencySelect.value === "dollar") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(inputCurrencyValue / dollarToday);
+
+        currencyNameMoeda.innerHTML = "Dollar";
+        currencyLogoMoeda.src = "./img/dollar.png";
+    }
+
+    if (currencySelect.value === "yuan") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("zh-CN", {
+            style: "currency",
+            currency: "CNY"
+        }).format(inputCurrencyValue / yuanToday);
+
+        currencyNameMoeda.innerHTML = "Yuan";
+        currencyLogoMoeda.src = "./img/yuan.png";
+    }
+
+    if (currencySelect.value === "bitcoin") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "BTC"
+        }).format(inputCurrencyValue / bitcoinToday);
+
+        currencyNameMoeda.innerHTML = "Bitcoin";
+        currencyLogoMoeda.src = "./img/bitcoin.png";
+    }
+
+    if (currencySelect.value === "real") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "BRL"
+        }).format(inputCurrencyValue);
+
+        currencyNameMoeda.innerHTML = "Real";
+        currencyLogoMoeda.src = "./img/brasil.png";
+    }
+
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(inputCurrencyValue);
+
+}
+
+//Anima a imagem de transação ao clicar no botão de conversão
+const currencyImg = document.querySelector(".img-cambio");
+convertButton.addEventListener("click", function () {
+    currencyImg.classList.add("fade-out");
+
+    setTimeout(() => {
+        currencyImg.src = "./img/transacao.gif";
+        currencyImg.classList.remove("fade-out");
+    }, 150);
+
+    setTimeout(() => {
+        currencyImg.classList.add("fade-out");
+        setTimeout(() => {
+            currencyImg.src = "./img/transacao-estatica.png";
+            currencyImg.classList.remove("fade-out");
+        }, 150);
+    }, 1500);
+});
+
+convertButton.addEventListener("click", convertCurrency);
