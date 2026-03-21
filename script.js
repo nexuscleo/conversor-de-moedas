0373d362be393db3d3a6223df6fb7fbc45e95cfa
CleomarSilva <nexuscleo@gmail.com>
2026-03-20 22:59:59 -0300

    refatora a função de conversão de moedas para utilizar configuração de moedas e melhorar a legibilidade

--- script.js
+++ script.js
@@ -2,131 +2,96 @@ const botaoConverter = document.querySelector(".botao-converter");
 const converterDe = document.querySelector(".converter-de");
 const converterPara = document.querySelector(".converter-para");
 
-async function convertCurrency() {
-    const valorEntrada = document.querySelector(".valor-entrada").value;
+const moedasConfig = {
+    "real": {
+        code: "BRL",
+        name: "Real",
+        locale: "pt-BR",
+        img: "./img/brasil.png"
+    },
+    "dollar": {
+        code: "USD",
+        name: "Dólar Americano",
+        locale: "en-US",
+        img: "./img/dollar.png"
+    },
+    "euro": {
+        code: "EUR",
+        name: "Euro",
+        locale: "de-DE",
+        img: "./img/euro.png"
+    },
+    "yuan": {
+        code: "CNY",
+        name: "Yuan Chinês",
+        locale: "zh-CN",
+        img: "./img/yuan.png"
+    },
+    "bitcoin": {
+        code: "BTC",
+        name: "Bitcoin",
+        locale: "en-US",
+        img: "./img/bitcoin.png"
+    }
+};
+
+async function converterMoeda() {
+    const valorEntradaRaw = document.querySelector(".valor-entrada").value;
+    const valorEntrada = parseFloat(valorEntradaRaw);
+    
+    if (isNaN(valorEntrada) || valorEntrada <= 0)
+        alert("Por favor, insira um valor válido.");
+        return;
+    
     const valorMoedaDe = document.querySelector(".valor-moeda-de");
     const valorMoedaPara = document.querySelector(".valor-moeda-para");
-
-    const data = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,CNY-BRL,BTC-BRL").then(response => response.json());
-
-    const dollarToday = data.USDBRL.high;
-    const euroToday = data.EURBRL.high;
-    const yuanToday = data.CNYBRL.high;
-    const bitcoinToday = data.BTCBRL.high;
-    const realToday = 1;
-
     const nomeMoedaDe = document.querySelector(".nome-moeda-de");
     const logoMoedaDe = document.querySelector(".logo-moeda-de");
     const nomeMoedaPara = document.querySelector(".nome-moeda-para");
     const logoMoedaPara = document.querySelector(".logo-moeda-para");
 
-    if (converterDe.value == "dollar") {
-        valorMoedaDe.innerHTML = new Intl.NumberFormat("en-US", {
-            style: "currency",
-            currency: "USD"
-        }).format(valorEntrada);
+    const configDe = moedasConfig[converterDe.value];
+    const configPara = moedasConfig[converterPara.value];
 
-        nomeMoedaDe.innerHTML = "Dólar Americano";
-        logoMoedaDe.src = "./img/dollar.png";
+    let cotacao = 1;
+
+    if (configDe.De !== configPara.code) {
+        try {
+            const par = `${configDe.code}-${configPara.code}`;
+            const url = `https://economia.awesomeapi.com.br/last/${par}`;
+            const response = await fetch(url);
+
+            if (!response.ok) {
+                throw new Error("Par de não suportado, erro na API");
+            }
+            const data = await response.json();
+            const chaveApi = `${configDe.code}${configPara.cade}`;
+            cotacao = parseFloat(data[chaveApi].high);
+        }
+        catch (error) {
+            console.error(error);
+            alert("Não foi possível obter a cotação para as moedas selecionadas.");
+            return;
+        }
     }
 
-        if (converterDe.value == "euro") {
-        valorMoedaDe.innerHTML = new Intl.NumberFormat("de-DE", {
-            style: "currency",
-            currency: "EUR"
-        }).format(valorEntrada);
+    const valorConvertido = valorEntrada * cotacao;
 
-        nomeMoedaDe.innerHTML = "Euro";
-        logoMoedaDe.src = "./img/euro.png";
-    }
-
-    if (converterDe.value == "yuan") {
-        valorMoedaDe.innerHTML = new Intl.NumberFormat("zh-CN", {
-            style: "currency",
-            currency: "CNY"
-        }).format(valorEntrada);
-
-        nomeMoedaDe.innerHTML = "Yuan";
-        logoMoedaDe.src = "./img/yuan.png";
-    }
-
-    if (converterDe.value == "bitcoin") {
-        valorMoedaDe.innerHTML = new Intl.NumberFormat("en-US", {
-            style: "currency",
-            currency: "BTC"
-        }).format(valorEntrada);
-
-        nomeMoedaDe.innerHTML = "Bitcoin";
-        logoMoedaDe.src = "./img/bitcoin.png";
-    }
-
-    if (converterDe.value == "real") {
-        valorMoedaDe.innerHTML = new Intl.NumberFormat("pt-BR", {
-            style: "currency",
-            currency: "BRL"
-        }).format(valorEntrada);
-
-        nomeMoedaDe.innerHTML = "Real";
-        logoMoedaDe.src = "./img/brasil.png";
-    }
-
-    /* Valores convertidos */
-    if (converterPara.value == "dollar") {
-        valorMoedaPara.innerHTML = new Intl.NumberFormat("en-US", {
-            style: "currency",
-            currency: "USD"
-        }).format(valorEntrada / dollarToday);
-
-        nomeMoedaPara.innerHTML = "Dólar Americano";
-        logoMoedaPara.src = "./img/dollar.png";
-    }
-
-    if (converterPara.value == "euro") {
-        valorMoedaPara.innerHTML = new Intl.NumberFormat("de-DE", {
-            style: "currency",
-            currency: "EUR"
-        }).format(valorEntrada / euroToday);
-
-        nomeMoedaPara.innerHTML = "Euro";
-        logoMoedaPara.src = "./img/euro.png";
-    }
-
-    if (converterPara.value == "yuan") {
-        valorMoedaPara.innerHTML = new Intl.NumberFormat("zh-CN", {
-            style: "currency",
-            currency: "CNY"
-        }).format(valorEntrada / yuanToday);
-
-        nomeMoedaPara.innerHTML = "Yuan";
-        logoMoedaPara.src = "./img/yuan.png";
-    }
-
-    if (converterPara.value == "bitcoin") {
-        valorMoedaPara.innerHTML = new Intl.NumberFormat("en-US", {
-            style: "currency",
-            currency: "BTC"
-        }).format(valorEntrada / bitcoinToday);
-
-        nomeMoedaPara.innerHTML = "Bitcoin";
-        logoMoedaPara.src = "./img/bitcoin.png";
-    }
-
-    if (converterPara.value == "real") {
-        valorMoedaPara.innerHTML = new Intl.NumberFormat("pt-BR", {
-            style: "currency",
-            currency: "BRL"
-        }).format(valorEntrada / realToday);
-
-        nomeMoedaPara.innerHTML = "Real";
-        logoMoedaPara.src = "./img/brasil.png";
-    }
-
-    /*currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+    nomeMoedaDe.innerHTML = configDe.name;
+    logoMoedaDe.src = configDe.img;
+    valorMoedaDe.innerHTML = new Intl.NumberFormat(configDe.locale, {
         style: "currency",
-        currency: "BRL"
-    }).format(inputCurrencyValue);*/
+        currency: configDe.code
+    }).format(valorEntrada);
+
+    nomeMoedaPara.innerHTML = configPara.name;
+    logoMoedaPara.src = configPara.img;
+    valorMoedaPara.innerHTML = new Intl.NumberFormat(configPara.locale, {
+        style: "currency",
+        currency: configPara.code
+    }).format(valorConvertido);
 }
 
-converterDe.addEventListener("change", convertCurrency);
-converterPara.addEventListener("change", convertCurrency);
-botaoConverter.addEventListener("click", convertCurrency);
+converterDe.addEventListener("change", converterMoeda);
+converterPara.addEventListener("change", converterMoeda);
+botaoConverter.addEventListener("click", converterMoeda);
